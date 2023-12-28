300b50fbde6b2752fe6a5eba35741a58e710a6bf
Mariana Vanucci <marianavanucciandrade@gmail.com>
2023-12-28 16:28:16 -0300

    set Layout component

---
+++ pages/components/Layout.js
@@ -0,0 +1,17 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Head from "next/head";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico"></link>
+        <title>PokeNext</title>
+      </Head>
+      <Navbar></Navbar>
+      <main>{children}</main>
+      <Footer></Footer>
+    </>
+  );
+}
